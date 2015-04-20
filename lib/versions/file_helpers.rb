99770d262657550960b3b13f7e6f5706fe6620d7
Sean Callan <seancallan@gmail.com>
2015-04-19 18:35:51 -0700

    Update filesystem and version mapping

--- lib/versions/file_helpers.rb
+++ lib/versions/file_helpers.rb
@@ -2,35 +2,30 @@ class Versions
   module FileHelpers
     protected
 
-    # Internal: Gets the path to the library's versions
-    #
-    # Return a directory path
-    def library_path
-      @path ||= Versions.config.base_dir
-      File.join(@path, @library_name)
-    end
-
     # Internal: Parse a file path into a version string
     #
     # path - the file path to parse
     #
     # Returns a version String or nil
     def parse_filepath(path)
-      name   = File.basename(path, '.rb')
-      (match = Versions.config.filename_pattern.match(name)) ? match[1] : nil
+      (match = Versions.config.version_pattern.match(path)) ? match[1] : nil
     end
 
     # Internal: Create a map of versions and files
     #
     # Returns a Hash
     def map_file_versions
-      files    = Dir[File.join(library_path, '*.rb')]
-      files    = {} if files.empty?
-      files.inject({}) do |memo, f| 
-        version_key      = parse_filepath(f)
-        memo[version_key.to_sym] = f if version_key
-        memo 
+      file_path = File.join(path, '**', "#{@library_name}.rb")
+      files = Dir[file_path] || {}
+      files.inject({}) do |memo, path|
+        version_key = parse_filepath(path)
+        memo[version_key.to_sym] = path if version_key
+        memo
       end
+    end
+
+    def path
+      @path ||= Versions.config.base_dir
     end
   end
 end
